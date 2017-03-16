8958be6cc14cd9b37021cb6181404c8b8b86b0ad
Andrey Kravtsun <kravtsun@cvisionlab.com>
2017-03-17 00:54:05 +0300

    Optimized performance (from ~20s on n=1000 to ~0.7s).

--- knn/knn.py
+++ knn/knn.py
@@ -5,7 +5,7 @@ from collections import Counter
 
 
 def read_csv(filename):
-    m = np.genfromtxt(filename, delimiter=',', dtype=np.double, skip_header=1)[:1000, :]
+    m = np.genfromtxt(filename, delimiter=',', dtype=np.double, skip_header=1)
     return m;
 
 def euclidean(a, b):
@@ -21,33 +21,27 @@ def knn(data, k):
     assert nfeatures + 1 == data.shape[1]
 
     def test_sample(row):
-        cur_feature = row[:-1]
-        cur_label = int(row[-1])
+        # distances = np.apply_along_axis(euclidean, 1, features, b=cur_feature)
+        # calculate euclidian distances between samples and current feature vector.
+        distances = np.sqrt(np.sum((features - row) ** 2, axis=1))
 
-        f = partial(euclidean, cur_feature)
-        distances = np.apply_along_axis(f, 1, features)
-        # assert distances.shape == (nsamples,)
+        assert distances.shape == (nsamples,)
 
-        # can be bad when several points are one.
-        # but we can exclude this cases manually.
+        # can be bad when several points are one. but we can exclude this cases manually.
         best_knn_indexes = distances.argsort()[1:k+1]
-        best_knn_answers = list(answers[best_knn_indexes])
+        best_knn_answers = answers[best_knn_indexes]
 
-        return Counter(best_knn_answers).most_common(1)[0][0]
-        # cntr[cur_label] -= 1
-        # label = cntr.most_common(1)[0][0]
-        # return label
-    assumed_labels = np.apply_along_axis(test_sample, 1, data)
+        # requires integer labels starting from 0. to be replaced with np.unique otherwise.
+        return np.bincount(best_knn_answers).argmax() # as k is relatively small, this optimization has no future.
+
+    assumed_labels = np.apply_along_axis(test_sample, 1, features)
     b = np.not_equal(assumed_labels, answers)
     return float(sum(b)) / nsamples
 
 if __name__ == '__main__':
     import sys
     filename = sys.argv[1]
+    k = int(sys.argv[2])
     data = read_csv(filename)
-    # n = features.shape[0]
-    # assert n > 1
-    # print("accuracy is ", estimate_quality(knn_classifier, *test))
-    # print(float(sum(answers) / len(answers)))
-    print("LOO is ", knn(data, 5))
+    print(knn(data, k))
 
