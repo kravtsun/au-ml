f9d786fd36b1f6cda88fb356785be5f4486b7961
Andrey Kravtsun <kravtsun@cvisionlab.com>
2017-03-18 01:45:15 +0300

    Solving task6.

--- knn/sklearn_check.py
+++ knn/sklearn_check.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python
-import sys
+import argparse
+from operator import itemgetter
+from itertools import starmap
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import LeaveOneOut
-from collections import Counter
 from sklearn.preprocessing import normalize
 
-filename = sys.argv[1]
-k = int(sys.argv[2])
 
-v = np.loadtxt(filename, delimiter=',',skiprows=1)
-nsamples = v.shape[0]
-x = v[:, :-1]
-x = normalize(x, axis=0, norm='max')
-y = v[:, -1]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="run KNN classifier with given arguments")
+    parser.add_argument("-f", dest="filename", required=True, help="CSV file.")
+    parser.add_argument("-k", type=int, dest="k", required=True)
+    parser.add_argument("-n", "--normalize", dest="normalize", action="store_true", default=False)
+    args = parser.parse_args()
 
-neigh = NearestNeighbors(n_neighbors=k, metric='euclidean')
+    v = np.loadtxt(args.filename, delimiter=',', skiprows=1)
+    nsamples = v.shape[0]
+    x = v[:, :-1]
+    if args.normalize:
+        x = normalize(x, axis=0, norm='max')
+    y = v[:, -1]
 
-cnt = 0
-loo = LeaveOneOut()
+    neigh = KNeighborsClassifier(n_neighbors=args.k, metric='euclidean')
 
-for train_index, test_index in loo.split(x):
-    x_train, x_test = x[train_index], x[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    classifier = neigh.fit(x_train, y_train)
-    indexes = neigh.kneighbors(x_test, return_distance=False)
-    labels = y_train[indexes][0]
-    chosen_label = Counter(labels).most_common(1)[0][0]
-    assert len(y_test) == 1
-    cnt += chosen_label != y_test[0]
-
-print(float(cnt) / nsamples)
+    loo = LeaveOneOut()
+    def split_func(train_index, test_index):
+            x_train, x_test = x[train_index], x[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            neigh.fit(x_train, y_train)
+            labels = neigh.predict(x_test)
+            assert labels.shape == (1,)
+            return labels[0]
+    loo_split = list(loo.split(x, y))
+    predict_labels = list(starmap(split_func, loo_split))
+    test_indexes = zip(*map(itemgetter(1), loo_split))[0]
+    test_labels = y[np.array(test_indexes)]
+    missed_count = np.count_nonzero(test_labels != predict_labels)
+    print(float(missed_count) / nsamples)
