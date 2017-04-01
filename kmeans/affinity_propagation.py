0927c867616374b19217c071c3c433535e2ca1db
Andrey Kravtsun <kravtsun@cvisionlab.com>
2017-04-01 12:01:04 +0300

    Affinity propagation: speed optimization.

--- kmeans/affinity_propagation.py
+++ kmeans/affinity_propagation.py
@@ -16,7 +16,7 @@ def sklearn_affinity_propagation(data, pref=None, damping=0.80):
     print pref, n_clusters_
     return labels
 
-def affinity_propagation(data, pref=None, w=0.5, iterations=100, EPS=1e-3):
+def affinity_propagation(data, pref=None, w=0.5, iterations=300, EPS=1e-3):
     n = data.shape[0]
     s = np.apply_along_axis(lambda p: similarity(p, data), 1, data)
     s = np.ma.masked_array(s, mask=False)
@@ -33,29 +33,25 @@ def affinity_propagation(data, pref=None, w=0.5, iterations=100, EPS=1e-3):
     ra = np.diag(r) + np.diag(a)
     rpositive = np.ma.zeros(shape=(n,n))
     rpositive.mask = False
+    natural_index = np.arange(n)
     while it < iterations or np.sum(np.diag(a + r) > 0) == 0:
         it += 1
         oldr = r.copy()
-        for k in range(n):
-            r[:, k] = s[:, k]
-            assert id(r[0, k]) != s[0, k]
-            a.mask[:, k] = True
-            s.mask[:, k] = True
-            r[:, k] -= np.max(a + s, axis=1)
-            s.mask[:, k] = False
-            a.mask[:, k] = False
-        r = w * oldr + (1 - w) * r
-        rpositive.data[:] = np.maximum(0, r)
-        rpositive.data.flat[::n+1] = r.flat[::n+1]
         olda = a.copy()
-
-        for i in range(n):
-            rpositive.mask[i, :] = True
-            rpositive_sum = np.sum(rpositive, axis=0)
-            a[i, :] = np.minimum(0, rpositive_sum)
-            a[i, i] = rpositive_sum[i]
-            rpositive.mask[i, :] = False
-        a = w * olda + (1 - w) * a
+        assum = a + s
+        first_max_index = np.argmax(assum, axis=1) # I
+        first_max = assum[natural_index, first_max_index].copy() # Y
+        assum[natural_index, first_max_index] = -np.inf
+        second_max = np.max(assum, axis=1)
+        r = s - first_max[:, np.newaxis]
+        r[natural_index, first_max_index] = s[natural_index, first_max_index] - second_max
+        r = (1 - w) * r + w * oldr
+        rpositive = np.maximum(r, 0)
+        rpositive.flat[::n + 1] = r.flat[::n + 1]
+        rpositive -= np.sum(rpositive, axis=0)
+        a = np.clip(rpositive, 0, np.inf)
+        a.flat[::n + 1] = np.diag(rpositive)
+        a = w * olda - (1 - w) * a
         dist = distance(np.diag(olda) + np.diag(oldr), np.diag(a) + np.diag(r))
         print "dist = ", dist
         if dist < EPS and np.sum(np.diag(a + r) > 0) > 0:
@@ -64,7 +60,6 @@ def affinity_propagation(data, pref=None, w=0.5, iterations=100, EPS=1e-3):
     iexemplars = np.where(np.diag(a + r) > 0)[0]
     clusters = np.argmax(s[:, iexemplars], axis=1)
     clusters[iexemplars] = np.arange(n)
-    # labels = iexemplars[clusters]
     return clusters
 
 
