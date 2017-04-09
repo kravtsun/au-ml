35fc2604012930389120c7351496722dfc62899f
Andrey Kravtsun <kravtsun@cvisionlab.com>
2017-04-09 13:49:40 +0300

    Finished working on hw1.

--- knn/knn.py
+++ knn/knn.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import numpy as np
 from functools import partial
 from collections import Counter
 import argparse
+from sklearn.neighbors import KDTree
 
 
 def read_csv(filename):
@@ -37,14 +38,44 @@ def precalc_data(data):
     return distances
 
 
-def knn(data, knn_filter, distances):
+def knn_with_kdtree(data, k):
     features = get_features(data)
     answers = data[:, -1]
     nsamples = data.shape[0]
     assert nsamples == answers.shape[0]
     nfeatures = features.shape[1]
     assert nfeatures + 1 == data.shape[1]
-    assert distances.shape == (nsamples, nsamples)
+
+    most_common_label = Counter(answers).most_common(1)[0][0]
+    tree = KDTree(features, leaf_size=2)
+    def test_sample(row_data):
+        assert row_data.shape == (nfeatures + 1,)
+        row_label = row_data[-1]
+        row_features = row_data[:-1]
+        best_knn_distances, best_knn_indexes = tree.query(row_features.reshape(1, -1), k=k+1)
+        best_knn_answers = answers[best_knn_indexes.flatten()]
+        counter = Counter(list(best_knn_answers))
+        counter.subtract([row_label])
+        most_common = counter.most_common(1)
+        if len(most_common) == 0 or most_common[0][1] == 0:
+            # can't classify: no elements in the neighborhood.
+            return most_common_label
+        else:
+            return most_common[0][0]
+
+    assumed_labels = np.array([test_sample(row_data) for row_data in data])
+    assert assumed_labels.shape == (nsamples,)
+    b = np.not_equal(assumed_labels, answers)
+    return float(sum(b)) / nsamples
+
+
+def knn(data, knn_filter, distances=None, kdtree=None):
+    features = get_features(data)
+    answers = data[:, -1]
+    nsamples = data.shape[0]
+    assert nsamples == answers.shape[0]
+    nfeatures = features.shape[1]
+    assert nfeatures + 1 == data.shape[1]
 
     most_common_label = Counter(answers).most_common(1)[0][0]
 
@@ -54,7 +85,6 @@ def knn(data, knn_filter, distances):
         row_label = row_data[-1]
         best_knn_indexes = knn_filter(row_distances)
         best_knn_answers = answers[best_knn_indexes]
-
         counter = Counter(list(best_knn_answers))
         counter.subtract([row_label])
         most_common = counter.most_common(1)
@@ -83,6 +113,7 @@ def main(argv, data=None, distances=None):
     def create_args_parser():
         parser = argparse.ArgumentParser(description="run KNN classifier with given arguments")
         parser.add_argument("-f", dest="filename", required=data is None)
+        parser.add_argument("--kdtree", dest="kdtree", action='store_true', required=False, default=False)
         knn_param_group = parser.add_mutually_exclusive_group(required=True)
         knn_param_group.add_argument("-k", type=int, dest="k")
         knn_param_group.add_argument("-r", type=float, dest="r")
@@ -91,22 +122,24 @@ def main(argv, data=None, distances=None):
     parser = create_args_parser()
     args = parser.parse_args(argv)
 
+    if data is None:
+        data = read_csv(args.filename)
+    if args.normalize:
+        normalize_data(data)
+
+    if args.kdtree:
+        assert args.k is not None
+        return knn_with_kdtree(data, args.k)
+
     knn_filter = None
     if args.k:
         knn_filter = partial(knn_filter_distance_by_number, k=args.k)
     else:
         knn_filter = partial(knn_filter_distance_by_radius, r=args.r)
 
-    if data is None:
-        data = read_csv(args.filename)
-
-    if args.normalize:
-        normalize_data(data)
-
     if distances is None:
         distances = precalc_data(data)
-
-    return knn(data, knn_filter, distances)
+    return knn(data, knn_filter, distances, kdtree=args.kdtree)
 
 
 if __name__ == '__main__':
